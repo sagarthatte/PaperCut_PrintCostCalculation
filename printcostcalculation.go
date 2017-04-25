1e7744e35795b909c3f098fa518d0e03a8aab431
sagarthatte <sagarthatte@gmail.com>
2017-04-25 21:19:58 +1000

    Create function for 'print cost calculation'
    Created a reusable function for calculating printing costs.
    Removed redundant code from previous procedural approach
    Created Map for printing cost data.

--- printcostcalculation.go
+++ printcostcalculation.go
@@ -8,18 +8,8 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
-	"math"
 	"strconv"
 	"strings"
-)
-
-//defining printing costs as constant values
-const (
-	oneSidedBWCost    float64 = 0.15
-	oneSidedColorCost float64 = 0.25
-	twoSidedBWCost    float64 = 0.10
-	twoSidedColorCost float64 = 0.20
-	//csvHasHeader      bool    = true
 )
 
 //error checking function
@@ -29,16 +19,39 @@ func check(e error) {
 	}
 }
 
-func round(num float64) int {
-	return int(num + math.Copysign(0.55, num))
-}
+//function to calculate printing costs
+func calculatePrintJobCost(costDict map[string]float64, printData []string) float64 {
+	var currentPrintJobCost float64
+	totalPages, err := strconv.ParseFloat(printData[0], 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	colorPages, err := strconv.ParseFloat(printData[1], 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bwPages := (totalPages - colorPages)
+	isTwoSided, err := strconv.ParseBool(printData[2])
 
-func toFixed(num float64, precision int) float64 {
-	output := math.Pow(10, float64(precision))
-	return float64(round(num*output)) / output
+	if isTwoSided == true {
+		currentPrintJobCost := (bwPages * costDict["twoSidedBWCost"])
+		currentPrintJobCost += (colorPages * costDict["twoSidedColorCost"])
+		return currentPrintJobCost
+	} else if isTwoSided == false {
+		currentPrintJobCost := (bwPages * costDict["oneSidedBWCost"])
+		currentPrintJobCost += (colorPages * costDict["oneSidedColorCost"])
+		return currentPrintJobCost
+	}
+	return currentPrintJobCost
 }
 
 func main() {
+	a4PrintCosts := make(map[string]float64)
+	a4PrintCosts["oneSidedBWCost"] = 0.15
+	a4PrintCosts["oneSidedColorCost"] = 0.25
+	a4PrintCosts["twoSidedBWCost"] = 0.10
+	a4PrintCosts["twoSidedColorCost"] = 0.20
+
 	csvHasHeader := true
 	totalPrintJobCost := 0.0
 	dat, err := ioutil.ReadFile("sample.csv")
@@ -59,28 +72,9 @@ func main() {
 		if csvHasHeader == true {
 			csvHasHeader = false
 		} else {
-			totalPages, err := strconv.ParseFloat(record[0], 32)
-			if err != nil {
-				log.Fatal(err)
-			}
-			colorPages, err := strconv.ParseFloat(record[1], 32)
-			if err != nil {
-				log.Fatal(err)
-			}
-			bwPages := (totalPages - colorPages)
-			isTwoSided, err := strconv.ParseBool(record[2])
-
-			if isTwoSided == true {
-				currentPrintJobCost := (bwPages * twoSidedBWCost)
-				currentPrintJobCost += (colorPages * twoSidedColorCost)
-				fmt.Printf("Cost for current print job is: $%.2f \n", currentPrintJobCost)
-				totalPrintJobCost += currentPrintJobCost
-			} else if isTwoSided == false {
-				currentPrintJobCost := (bwPages * oneSidedBWCost)
-				currentPrintJobCost += (colorPages * oneSidedColorCost)
-				fmt.Printf("Cost for current print job is: $%.2f \n", currentPrintJobCost)
-				totalPrintJobCost += currentPrintJobCost
-			}
+			currentPrintJobCost := calculatePrintJobCost(a4PrintCosts, record)
+			fmt.Printf("Cost for current print job is: $%.2f \n", currentPrintJobCost)
+			totalPrintJobCost += currentPrintJobCost
 		}
 	}
 	fmt.Printf("Total Print Cost is: $%.2f", totalPrintJobCost)
